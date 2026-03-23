4ba04f9e38037bb9e734318fa93ef6dcf82d06df
Dominik Agres <dominik@agres.online>
2026-03-23 19:44:55 +0100

    fix: class name

--- Formula/cheatsh.rb
+++ Formula/cheatsh.rb
@@ -1,9 +1,9 @@
-class Myapp < Formula
+class cheatsh < Formula
   desc    "a terminal cheat sheet for linux"
   homepage "https://github.com/agresdominik/cheat_sheet"
   url     "https://github.com/agresdominik/cheat_sheet/archive/refs/tags/v#{version}.tar.gz"
   version "0.0.1"                                    
-  sha256  "TODO_SHA256"                              # run: curl -sL <url> | sha256sum
+  sha256  "0000000000000000000000000000000000000000000000000000000000000000"                              
   license "GPL-3.0"                                      
 
   depends_on "go" => :build
