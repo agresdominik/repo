feff09c7828289cdd8d6cadeae0576c5abb98e53
Dominik Agres <dominik@agres.online>
2026-03-23 19:47:56 +0100

    fix: sha sum

--- Formula/cheatsh.rb
+++ Formula/cheatsh.rb
@@ -3,7 +3,7 @@ class Cheatsh < Formula
   homepage "https://github.com/agresdominik/cheat_sheet"
   version "0.1.0"                                    
   url     "https://github.com/agresdominik/cheat_sheet/archive/refs/tags/v#{version}.tar.gz"
-  sha256  "0000000000000000000000000000000000000000000000000000000000000000"                              
+  sha256  "377e949fcb2614ca1e25add3db6d27f221bec655179a323219535068d8185204"                              
   license "GPL-3.0"                                      
 
   depends_on "go" => :build
